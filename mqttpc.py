63ae1d8b421c88c8ab09f806c398255bb0535792
Mani Shankar <manishankar.offl@gmail.com>
2021-11-21 22:18:12 +0530

    Added Multiple message Publish and Subscribe.

--- mqttpc.py
+++ mqttpc.py
@@ -1,11 +1,32 @@
 import paho.mqtt.publish as publish
+import datetime
+import keyboard
+import time
+import random
+import json
  
 MQTT_SERVER = "192.168.1.18"
 MQTT_PATH = "channel"
 SUBPATH = ["Temp", "Vol", "Visc"]
-import time
+
+
+def append_datetime(msgs):
+    now = datetime.datetime.now().strftime("%d-%b-%Y %H:%M:%S")    
+    for topic in msgs:
+        
+        if type(topic["payload"]) is str:
+            topic["payload"] = json.loads(topic["payload"])        
+        topic["payload"]["datetime"] = now
+        topic["payload"] = json.dumps(topic["payload"])                
+    return msgs
+
 while True:
-    publish.single(MQTT_PATH + "/" + SUBPATH[0], "Temperature is 25", hostname=MQTT_SERVER) #send data continuously every 3 seconds
-    publish.single(MQTT_PATH + "/" + SUBPATH[1], "Volume is 84", hostname=MQTT_SERVER) #send data continuously every 3 seconds
-    publish.single(MQTT_PATH + "/" + SUBPATH[2], "Visc is 25", hostname=MQTT_SERVER) #send data continuously every 3 seconds
-    time.sleep(3)
+    msgs= [{"topic": "channel/Temp", "payload": {"data": "Temperature is {}".format(random.randrange(0, 100)), "datetime": None}},
+        {"topic": "channel/Vol", "payload": {"data": "Volume is {}".format(random.randrange(0, 100)),"datetime": None}},
+        {"topic": "channel/Visc", "payload": {"data": "Viscosity is {}".format(random.randrange(0, 100)), "datetime": None}} ]
+    
+    append_datetime(msgs)        
+    publish.multiple(msgs, hostname=MQTT_SERVER, keepalive=60)
+    time.sleep(0.5)
+    if keyboard.is_pressed("q"):
+        exit(0)
--- mqttpi.py
+++ mqttpi.py
@@ -1,7 +1,10 @@
 import paho.mqtt.client as mqtt #import library
+import json
  
 MQTT_SERVER = "localhost" #specify the broker address, it can be IP of raspberry pi or simply localhost
 MQTT_PATH = "channel/Temp" #this is the name of topic, like temp
+
+req_list = ["channel/Temp", "channel/Vol"]
  
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -9,11 +12,13 @@ def on_connect(client, userdata, flags, rc):
  
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(MQTT_PATH)
+    for topic in req_list:
+        client.subscribe(topic)
  
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    payload = json.loads(msg.payload)
+    print(msg.topic +" "+ str(payload["datetime"]) + " " + str(payload["data"]))    
     # more callbacks, etc
  
 client = mqtt.Client()
